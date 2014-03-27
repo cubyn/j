f2bb05f9d23c87fc6efa7ba3fb2389ee79fa228f
Luke Plaster <notatestuser@gmail.com>
2014-03-27 12:05:49 +0100

    Added and exported readFileAsync

--- j.js
+++ j.js
@@ -10,6 +10,30 @@ var utils_exts = require('./spotme-utils-extensions');
 utils_exts.extendXLS  (XLS);
 utils_exts.extendXLSX (XLSX);
 
+var readFileAsync = function(filename, options, callback) {
+    fs.open(filename, 'r', function(status, fd) {
+        if (status) {
+            console.error('fd status exception', status.message);
+            return callback(error(status.message));
+        }
+        var buffer = new Buffer(1);
+        fs.read(fd, buffer, 0, 1, 0, function(err, num) {
+            if (err) {
+                return callback(err);
+            }
+            switch(buffer[0]) {
+                /* CFB container */
+                case 0xd0: return callback(null, [XLS,   XLS.readFile(filename, options)]);
+                /* XML container (assumed 2003/2004) */
+                case 0x3c: return callback(null, [XLS,   XLS.readFile(filename, options)]);
+                /* Zip container */
+                case 0x50: return callback(null, [XLSX, XLSX.readFile(filename, options)]);
+            }
+            return callback(error('unrecognized file type'), [undefined, buffer]);
+        });
+    });
+};
+
 var readFileSync = function(filename, options) {
 	var f = fs.readFileSync(filename);
 	switch(f[0]) {
@@ -136,8 +160,9 @@ function to_clit(w, s) {
 
 module.exports = {
 	XLSX: XLSX,
-	XLS: XLS,
-	readFile:readFileSync,
+	XLS:  XLS,
+	readFile:      readFileSync,
+    readFileAsync: readFileAsync,
 	utils: {
 		to_csv: to_dsv,
 		to_dsv: to_dsv,
